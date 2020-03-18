be60c05fec82523b57f858e39e44246a327cc791
Babith <babithg@gmail.com>
2020-03-18 16:55:22 +0530

    updated hello.py with python script

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print ("Hello Word from pavans project")
+import os
+print ("Hello Word from pavans project forked by Babith")
+print("**"*10)
+print("Script running with details")
+os.system('top -n 1')
+print("**"*10)
+
