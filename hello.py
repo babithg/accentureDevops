44ea2856dd16944a5247864cea68cc5937f37106
Babith <babithg@gmail.com>
2020-03-18 17:03:23 +0530

    updated the script with print statement

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ import os
 print ("Hello Word from pavans project forked by Babith")
 print("**"*10)
 print("Script running with details")
-os.system('top -n 1')
+print(os.system('top -n 1'))
 print("**"*10)
 
